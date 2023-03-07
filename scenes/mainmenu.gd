9ab16410a42e9fcb9dc999c9a899e28e5a2d1e07
Erik Bernoth <erik.bernoth@gmail.com>
2023-03-08 00:18:37 +0100

    putting the player in its own map
    and resetting the start menu script
    
    Signed-off-by: Erik Bernoth <erik.bernoth@gmail.com>

--- scenes/mainmenu.gd
+++ scenes/mainmenu.gd
@@ -9,3 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://scenes/map_1.tscn")
