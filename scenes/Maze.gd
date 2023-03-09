4e502c5b4afdcf991d7151e9b616f489b7df2161
Erik Bernoth <erik.bernoth@gmail.com>
2023-03-09 23:45:19 +0100

    danger, unlimited loop
    Added some more spaghetti code. Still I feel it's going in the right
    direction, and there's nobody else who should get screwed by this.
    
    Signed-off-by: Erik Bernoth <erik.bernoth@gmail.com>

--- scenes/Maze.gd
+++ scenes/Maze.gd
@@ -6,6 +6,16 @@ extends TileMap
 
 const EMTPY_TILE = Vector2i(-1, -1)
 
+func wait(wtime: int):
+	#lol
+	var t = Timer.new()
+	t.set_wait_time(wtime)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	t.queue_free()
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# TODO: change to Vector2i
@@ -19,28 +29,29 @@ func _ready():
 	# 2. choose current from stack.pop() else end map generation
 	while stack:
 		var current: Vector2i = stack.pop_back()
-		print(current)
-		print(self.get_cell_atlas_coords(1, current))
-		print(current + Vector2i.RIGHT)
-		print(self.get_cell_atlas_coords(1, (current + Vector2i.RIGHT)))
-		print(self.get_surrounding_cells(current))
-		#self.set_cell(1, current,0,Vector2i(1,3),0)
-		print("after setting current cell")
-		print(self.get_cell_atlas_coords(1, current))
-		print(self.get_surrounding_cells(current))
 		var next: Vector2i
-		return
 		# 3. choose random neighbour until neighbour not visited else remove current from stack
 		var neighbours = self.get_surrounding_cells(current)
 		while neighbours:
+			wait(1)
+			print("current neighbours: ", neighbours)
 			next = neighbours.pop_at(randi() % neighbours.size())
+			stack.append(next)
+			print("check neighbour", next)
 			if next not in visited.keys():
 				# 3. create neighbor with open-back if doesn't exist else: open-back of neighbour
 				if self.get_cell_atlas_coords(1, next) == EMTPY_TILE:
-					# TODO: draw the tile, i.e. draw walls around it where necessary or take some away
-					pass
-				break
-	self.get_cell 
+					# move 2 steps ahead, so the wall tile can be used to separate paths
+					var direction = next - current
+					var nextnext = next + direction
+					self.set_cell(1, current,0,Vector2i(0,0),0)
+					self.set_cell(1, next,0,Vector2i(0,0),0)
+					self.set_cell(1, nextnext,0,Vector2i(0,0),0)
+					visited[current] = ""
+					visited[next] = ""
+					visited[nextnext] = ""
+					print("added tiles: ", current, next, nextnext)
+
 	# 4. open-forward of current tile
 	# 5. add neighbour to visited and stack
 	# 6. go to 2.
