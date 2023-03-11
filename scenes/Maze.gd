ea2c94f23956297f2be8f0684268d47ce3e1ff74
Erik Bernoth <erik.bernoth@gmail.com>
2023-03-11 12:59:00 +0100

    map generation algorithm done
    I found I needed a lot less tracking elements than I thought.
    One is a stack of tiles that might still have neighbours to explore
    left. And the other is the map itself. The only way a floor tile can be
    there, is because I put it there as a map creation step. So I know I
    have visited that spot.
    
    Also I'm quite happy about naming the function for drawing one specific
    floor tile excavate(). It is exactly how it visually feels when we use a
    timer to slow the drawing process: The tunnels are excavated into the
    dungeon.
    
    Next step would be to:
     - set start/end points for the dungeon
     - implement a level counter
     - collision with wall tiles
     - collision with map limits or resizing so there are certainly walls
       around it
     - maybe think about some kind of key-door thingy?
    
    Signed-off-by: Erik Bernoth <erik.bernoth@gmail.com>

--- scenes/Maze.gd
+++ scenes/Maze.gd
@@ -1,59 +1,61 @@
 extends TileMap
 
-@export var width = 25
-@export var height = 25
+@export var width = 18
+@export var height = 10
 @export var stepsize = 2
 
 const EMTPY_TILE = Vector2i(-1, -1)
+const FLOOR_TILE = Vector2i(3,0)
+const WALL_TILE = Vector2i(0,0)
 
-func wait(wtime: int):
-	#lol
-	var t = Timer.new()
-	t.set_wait_time(wtime)
-	t.set_one_shot(true)
-	self.add_child(t)
-	t.start()
-	t.queue_free()
+# check if the coordinates are actually on the map
+func on_map(to_check: Vector2i) -> bool:
+	return (to_check.x >= 0 and to_check.x < width
+			and to_check.y >= 0 and to_check.y < height)
+
+
+# set a floor tile and surrounding wall tiles correctly
+func excavate(to_excavate: Vector2i, source_id: int, floor_tile: Vector2i, wall_tile: Vector2i) -> void:
+	var neighbourhood = [
+		to_excavate,
+		to_excavate + Vector2i.UP + Vector2i.LEFT,
+		to_excavate + Vector2i.UP,
+		to_excavate + Vector2i.UP + Vector2i.RIGHT,
+		to_excavate + Vector2i.RIGHT,
+		to_excavate + Vector2i.DOWN + Vector2i.RIGHT,
+		to_excavate + Vector2i.DOWN,
+		to_excavate + Vector2i.DOWN + Vector2i.LEFT,
+		to_excavate + Vector2i.LEFT,
+	]
+	for current in neighbourhood:
+		if !on_map(current):
+			continue
+		if self.get_cell_atlas_coords(0, current) == floor_tile:
+			continue
+		self.set_cell(0, current, source_id,
+				floor_tile if current == to_excavate else wall_tile)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# TODO: change to Vector2i
-	# TODO: replace thoughts about set_cellv with set_cell/get_cell_??? stuff
-	# TODO: check TODOs below
-	# 1. choose starting tile and add to visited and add to stack
 	randomize()
-	var visited = {}
-	var stack = [Vector2i(randi() % width, randi() % height)]
-	
-	# 2. choose current from stack.pop() else end map generation
-	while stack:
-		var current: Vector2i = stack.pop_back()
-		var next: Vector2i
-		# 3. choose random neighbour until neighbour not visited else remove current from stack
-		var neighbours = self.get_surrounding_cells(current)
-		while neighbours:
-			wait(1)
-			print("current neighbours: ", neighbours)
-			next = neighbours.pop_at(randi() % neighbours.size())
-			stack.append(next)
-			print("check neighbour", next)
-			if next not in visited.keys():
-				# 3. create neighbor with open-back if doesn't exist else: open-back of neighbour
-				if self.get_cell_atlas_coords(1, next) == EMTPY_TILE:
-					# move 2 steps ahead, so the wall tile can be used to separate paths
-					var direction = next - current
-					var nextnext = next + direction
-					self.set_cell(1, current,0,Vector2i(0,0),0)
-					self.set_cell(1, next,0,Vector2i(0,0),0)
-					self.set_cell(1, nextnext,0,Vector2i(0,0),0)
-					visited[current] = ""
-					visited[next] = ""
-					visited[nextnext] = ""
-					print("added tiles: ", current, next, nextnext)
-
-	# 4. open-forward of current tile
-	# 5. add neighbour to visited and stack
-	# 6. go to 2.
-	
-	pass
+	# track nodes we could excavate from next
+	var excavated = [Vector2i(randi() % width, randi() % height)]
+	while excavated:
+		var current: Vector2i = excavated[-1]
+		var directions = [Vector2i.UP, Vector2i.RIGHT, Vector2i.DOWN, Vector2i.LEFT]
+		directions.shuffle()
+		while directions:
+			var direction = directions.pop_back()
+			if (self.get_cell_atlas_coords(0, current + stepsize * direction) != FLOOR_TILE
+					and on_map(current + stepsize * direction)):
+				for i in range(0, stepsize+1):
+					await get_tree().create_timer(0.02).timeout
+					self.excavate(current + i * direction, 0, Vector2i(3,0), Vector2i(0,0))
+				excavated.append(current + stepsize * direction)
+				break
+			if directions.size() == 0:
+				# in case all directions have been exhausted without an excavate()->break
+				# it means the excavated node is fully surrounded with other excavated tiles
+				excavated.pop_back()
+				break
