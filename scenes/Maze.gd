6958264c469f458acbd7625b14d2fd1197d4af89
Erik Bernoth <erik.bernoth@gmail.com>
2023-03-12 18:31:22 +0100

    start and endpoint for level
    I think I'll also add a door and key in other endpoints, so I can
    guarantee that the player has to move a little even when the start- and
    endpoint are close together.
    
    Then I'll probably work on making things beautiful and usable.
    
    Signed-off-by: Erik Bernoth <erik.bernoth@gmail.com>

--- scenes/Maze.gd
+++ scenes/Maze.gd
@@ -39,11 +39,25 @@ func random_odd_num(upper_bound: int) -> int:
 	var rand = randi() % ((upper_bound / 2) - 1)
 	return (rand * 2) + 1
 
+func only_one_path(point: Vector2i) -> bool:
+	var check_list = [
+		point + Vector2i.UP,
+		point + Vector2i.RIGHT,
+		point + Vector2i.DOWN,
+		point + Vector2i.LEFT,
+	]
+	var wallcount : int = 0
+	for neighbour in check_list:
+		if self.get_cell_atlas_coords(0, neighbour) == WALL_TILE:
+			wallcount+=1
+	return wallcount == 3
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
 	# track nodes we could excavate from next
 	var excavated = [Vector2i(random_odd_num(width), random_odd_num(height))]
+	var endpoints = []
 	while excavated:
 		var current: Vector2i = excavated[-1]
 		var directions = [Vector2i.UP, Vector2i.RIGHT, Vector2i.DOWN, Vector2i.LEFT]
@@ -60,5 +74,13 @@ func _ready():
 			if directions.size() == 0:
 				# in case all directions have been exhausted without an excavate()->break
 				# it means the excavated node is fully surrounded with other excavated tiles
-				excavated.pop_back()
+				var point = excavated.pop_back()
+				if only_one_path(point):
+					endpoints.append(point)
+				# I assume that here we have the end nodes. These are the places for start, end, keys,
+				# etc. I want to put a simple staircase there at first to check.
+				# However, my tile generation does not look for staircases. How to handle?
 				break
+	endpoints.shuffle()
+	self.set_cell(0,endpoints[0],0,Vector2i(1,0),0)
+	self.set_cell(0,endpoints[1],0,Vector2i(2,0),0)
